da0c174332e5b5dae8752db3fd643757a0db6fb7
chris <christopherkmoore1@gmail.com>
2024-04-25 15:33:33 -0700

    adds picture

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../images/Web-developer.svg';
+import img from '../images/MF1.jpeg';
 import { Link } from 'react-router-dom';
 
 const Intro = () => {
@@ -8,9 +8,6 @@ const Intro = () => {
                 <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6" id='about' >
 
                     <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="fade-up">
-                        <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
-                            <img alt="card img" className="rounded-t float-right" src={img} />
-                        </div>
                         <div className="flex-col my-4 text-center lg:text-left lg:my-0 lg:justify-end w-full lg:w-1/2 px-8" data-aos="zoom-in" data-aos-delay="500">
                             
                             <h3 className="text-3xl  text-mayra-green font-bold">We develop high quality bespoke web and mobile applications for organizations, institutions and SMEs</h3>
@@ -26,6 +23,11 @@ const Intro = () => {
                                 <svg className="w-4 h-4 ml-1 group-hover: translate-x-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                             </Link>
                         </div>
+
+                        <div className="lg:w-2/5 flex flex-col lg:mx-4 justify-center">
+                            <img alt="card img" className="rounded-2xl float-right" src={img} />
+                        </div>
+
                     </div>
                 </div>
         </>
