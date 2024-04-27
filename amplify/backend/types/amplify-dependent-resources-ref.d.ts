267f5ac9a77003938c839ce6ef3e8dc8e7e9d92f
chris <christopherkmoore1@gmail.com>
2024-04-27 10:06:29 -0700

    adds analytics

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "mayfloreser": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "mayfloreser": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  }
+}
